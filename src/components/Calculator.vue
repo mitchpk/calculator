7533ecb976676f9d8f487611dc73aacd7a456ba9
mitchpk <30470175+mitchpk@users.noreply.github.com>
2020-08-05 21:51:22 +0930

    setup

---
+++ src/components/Calculator.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      v-model="query"
+      type="text"
+      @keyup.enter="run"
+      placeholder="Enter an operation"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    run() {
+      this.query = this.query.split(" ").join("");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
